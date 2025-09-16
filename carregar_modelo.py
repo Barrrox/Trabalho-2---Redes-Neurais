4776a4e0d9f6e9483258db9bbddf0ce5340f96e8
Barrrox <matheusbarros173@gmail.com>
2025-09-16 17:02:15 -0300

    Repositorio criado

---
+++ carregar_modelo.py
@@ -0,0 +1,86 @@
+from tensorflow.keras.models import load_model
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+
+
+def carregar_modelo(QNT_TESTES):
+	##Parte 2: Carregando o Modelo e Fazendo Previsões
+
+	#1. Carregando o Modelo Salvo
+
+	# Carrega o modelo salvo
+	model = load_model('model.keras')
+
+	#2. Matriz de confusão do conjunto de treino
+
+	# Carrega o conjunto de dados MNIST para teste
+	x_train = np.load("imagens_treino.npy")
+	y_train = np.load("labels_treino.npy")
+
+	# Criar Xtest e Ytest aleatoriamente
+	x_test = []
+	y_test = []
+	for _ in range(QNT_TESTES):
+		imagem_index = random.randint(0, len(y_train) - 1)
+		x_test.append(x_train[imagem_index])
+		y_test.append(y_train[imagem_index])
+
+	x_test = np.array(x_test)
+	y_test = np.array(y_test)
+
+	# Normaliza os dados de teste
+	x_test = x_test / 255.0
+
+	# Faz previsões no conjunto de teste
+	y_pred = np.argmax(model.predict(x_test), axis=1)
+
+	# Calcula a matriz de confusão
+	conf_matrix = confusion_matrix(y_test, y_pred)
+
+	# Exibe a matriz de confusão
+	ConfusionMatrixDisplay(conf_matrix).plot(cmap='Blues')
+	print("Matriz de confusão exibida com sucesso.")
+
+	#3. Fazendo Previsões com o Modelo Carregado
+
+	# Simula uma nova entrada: seleciona uma imagem aleatória do conjunto de teste
+	random_idx = np.random.randint(0, x_test.shape[0])
+	new_input = x_test[random_idx].reshape(1, 255, 255, 3)  # Redimensiona para o formato esperado pelo modelo
+
+	# Faz a previsão
+	prediction = model.predict(new_input)
+	predicted_class = np.argmax(prediction)
+
+	# Obtém a classe original
+	original_class = y_test[random_idx]
+
+	# Exibe a imagem de entrada e as classes
+	plt.figure(figsize=(6, 3))
+	plt.subplot(1, 2, 1)
+	plt.imshow(new_input[0], cmap='gray')
+	plt.title(f'Classe Original: {original_class}')
+	plt.axis('off')
+
+	plt.subplot(1, 2, 2)
+	plt.bar([0, 1], [prediction[0, original_class], 1 - prediction[0, original_class]], color=['blue', 'red'])
+	plt.xticks([0, 1], ['Classe Predita', 'Probabilidade'])
+	plt.title(f'Classe Predita: {predicted_class}')
+
+	plt.show()
+
+	print(f"Classe prevista: {predicted_class}")
+	print(f"Classe original: {original_class}")
+
+
+def main():
+
+	# Quantidade de imagens de teste para criar a matriz de confusão
+	QNT_TESTES = 1000
+
+	carregar_modelo(QNT_TESTES)
+
+
+if __name__ == "__main__":
+	main()
---
+++ megaformat.py
@@ -0,0 +1,114 @@
+from random import random
+import numpy as np
+from pathlib import Path
+from time import time
+
+
+
+def formatImage(imagemDesf):
+	# definir pontos de incio e fim de um espaço de crop 1:1 aleatório
+	height, width, channels = imagemDesf.shape
+	startY = 0
+	startX = 0
+	outScale = 0
+
+	if height > width:
+		outScale = width
+		startX = 0
+
+		cropOffset = int((height - width) * random.random())
+		startY = cropOffset
+
+	if width > height:
+		outScale = height
+		startY = 0
+
+		cropOffset = int((width - height) * random.random())
+		startX = cropOffset
+
+
+	# criar imagem de saída
+	outRes = 255
+	output = np.zeros((outRes, outRes, 3), dtype=np.uint8)
+
+
+	for y in range(outRes):
+		for x in range(outRes):
+			output[y, x] = imagemDesf[int((y/outRes)*outScale) + startY, int((x/outRes)*outScale) + startX]
+
+	# FOI!
+	return output
+
+
+def getFolder(path: str) -> str:
+    parts = path.split('\\', 2)
+    return parts[0]
+
+#
+def formatDataSet(caminhoParaODataSet):
+	"""
+	Formata o dataset inteiro de imagens, salva e retorna em forma de uma lista de matrizes de tuplas RGB.
+
+	"""
+
+	# Dicionario usado para transformar classe em labels
+	classe_para_label = {
+		"Baroque": 0,
+		"Cubism": 1,
+		"Expressionism": 2,
+		"Impressionism": 3,
+		"Minimalism": 4,
+		"Post_Impressionism": 5,
+		"Realism": 6,
+		"Romanticism": 7,
+		"Symbolism": 8
+	}
+
+	diretorio_raiz = Path(caminhoParaODataSet)
+
+	lista_de_caminhos_imagens = []
+	lista_de_caminhos_imagens.extend(diretorio_raiz.rglob("*"))
+
+	lista_de_matrizes = []
+	lista_de_classes = []
+	print(f"Encontradas {len(lista_de_caminhos_imagens)} imagens.")
+
+
+	# Para cada imagem na pasta de imagens
+	for caminho_imagem in lista_de_caminhos_imagens:
+		try:
+			print(f"Processando imagem: {caminho_imagem}")
+
+			# # Abre a imagem usando o objeto Path diretamente
+			# imagem = cv2.imread(caminho_imagem)
+
+			# imagemFormatada = formatImage(imagem)
+
+			# # Converte para matriz NumPy e adiciona à lista
+			# imagemFormatada = np.array(imagemFormatada)
+
+			# lista_de_matrizes.append(imagemFormatada)
+			
+			# classe = getFolder(str(caminho_imagem))
+
+			# lista_de_classes.append(classe_para_label[classe])
+
+		except Exception as e:
+			print(f"Erro ao processar o arquivo {caminho_imagem}: {e}")
+	
+	np.save('imagens_treino.npy', lista_de_matrizes)
+	np.save('labels_treino.npy', lista_de_classes)
+
+	return lista_de_matrizes, lista_de_classes
+
+inicio = time.time()
+formatDataSet("./BaseDeDados")
+print(f"Tempo : {time.time() - inicio}s")
+# # abrir arquivo da imagem
+# image = cv2.imread("exp.jpg")
+
+# # mostrar imagem
+# cv2.imshow('Imagem', formatImage(image))
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
