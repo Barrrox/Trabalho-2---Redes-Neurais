f23aaca079ff220d1df6afda0734034b6e14442a
Barrrox <matheusbarros173@gmail.com>
2025-09-22 17:46:23 -0300

    Arquivos para o database Impressionismo (13k imgs)

---
+++ megaformat_impressionism.py
@@ -0,0 +1,114 @@
+from cv2 import imread
+from random import random
+import numpy as np
+from pathlib import Path
+from time import time
+
+
+
+def formatImage(imagemDesf):
+	# definir pontos de incio e fim de um espaço de crop 1:1 aleatório
+	height, width, channels = imagemDesf.shape
+	startY = 0
+	startX = 0
+	outScale = 0
+
+	if height > width:
+		outScale = width
+		startX = 0
+
+		cropOffset = int((height - width) * random())
+		startY = cropOffset
+
+	if width > height:
+		outScale = height
+		startY = 0
+
+		cropOffset = int((width - height) * random())
+		startX = cropOffset
+
+
+	# criar imagem de saída
+	outRes = 255
+	output = np.zeros((outRes, outRes, 3), dtype=np.uint8)
+
+
+	for y in range(outRes):
+		for x in range(outRes):
+			output[y, x] = imagemDesf[int((y/outRes)*outScale) + startY, int((x/outRes)*outScale) + startX]
+
+	# FOI!
+	return output
+
+
+def getFolder(path: str) -> str:
+    return Path(path).parent.name
+
+#
+def formatDataSet(caminhoParaODataSet):
+	"""
+	Formata o dataset inteiro de imagens, salva e retorna em forma de uma lista de matrizes de tuplas RGB.
+
+	"""
+
+	# Dicionario usado para transformar classe em labels
+	classe_para_label = {
+		"Baroque": 0,
+		"Cubism": 1,
+		"Expressionism": 2,
+		"Impressionism": 3,
+		"Minimalism": 4,
+		"Post_Impressionism": 5,
+		"Realism": 6,
+		"Romanticism": 7,
+		"Symbolism": 8
+	}
+
+	diretorio_raiz = Path(caminhoParaODataSet)
+
+	lista_de_caminhos_imagens = []
+	lista_de_caminhos_imagens.extend(diretorio_raiz.rglob("*"))
+
+	lista_de_matrizes = []
+	lista_de_classes = []
+	print(f"Encontradas {len(lista_de_caminhos_imagens)} imagens.")
+
+
+	# Para cada imagem na pasta de imagens
+	for caminho_imagem in lista_de_caminhos_imagens:
+		try:
+			print(f"Processando imagem: {caminho_imagem}")
+
+			# Abre a imagem usando o objeto Path diretamente
+			imagem = imread(caminho_imagem)
+
+			imagemFormatada = formatImage(imagem)
+
+			# Converte para matriz NumPy e adiciona à lista
+			imagemFormatada = np.array(imagemFormatada)
+
+			lista_de_matrizes.append(imagemFormatada)
+			
+			classe = getFolder(str(caminho_imagem))
+
+			lista_de_classes.append(classe_para_label[classe])
+
+		except Exception as e:
+			print(f"Erro ao processar o arquivo {caminho_imagem}: {e}")
+	
+	np.save('imagens_treino_impressionismo.npy', lista_de_matrizes)
+	np.save('labels_treino_impressionismo.npy', lista_de_classes)
+
+	return lista_de_matrizes, lista_de_classes
+
+inicio = time()
+formatDataSet("./Impressionism")
+print(f"Tempo : {time() - inicio}s")
+# # abrir arquivo da imagem
+# image = cv2.imread("exp.jpg")
+
+# # mostrar imagem
+# cv2.imshow('Imagem', formatImage(image))
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
