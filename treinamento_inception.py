d53e5cbe57043a9ed29b2c6d6678f7e0dc26b783
Barrrox <matheusbarros173@gmail.com>
2025-09-27 16:51:04 -0300

    Troca relu -> BN para BN -> relu apos Conv2D

--- treinamento_inception.py
+++ treinamento_inception.py
@@ -6,15 +6,15 @@ para classificar pinturas por movimento artístico.
 """
 
 import numpy as np
-from tensorflow.keras.models import Model
-from tensorflow.keras.layers import (
+from tensorflow.keras.models import Model # type: ignore
+from tensorflow.keras.layers import ( # type: ignore
     Dense, Conv2D, MaxPooling2D, Input,
     Concatenate, Dropout, BatchNormalization,
-    GlobalAveragePooling2D
-)
-from tensorflow.keras.utils import to_categorical
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+    GlobalAveragePooling2D, Activation
+) 
+from tensorflow.keras.utils import to_categorical # type: ignore
+from tensorflow.keras.preprocessing.image import ImageDataGenerator # type: ignore
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint # type: ignore
 from sklearn.model_selection import train_test_split
 from time import time
 
@@ -63,37 +63,56 @@ def treinar_modelo(TAM_TESTES, TAM_VALIDACAO, QNT_EPOCAS):
 
     # --- 3. Definição do Modelo ---
     def inception_module(x, f1, f2_in, f2_out, f3_in, f3_out, f4_out):
-        conv1 = Conv2D(f1, (1,1), padding='same', activation='relu')(x)
+        # Ramo 1
+        conv1 = Conv2D(f1, (1,1), padding='same', use_bias=False)(x)
+        conv1 = BatchNormalization()(conv1)
+        conv1 = Activation('relu')(conv1)
 
-        conv3_in = Conv2D(f2_in, (1,1), padding='same', activation='relu')(x)
-        conv3 = Conv2D(f2_out, (3,3), padding='same', activation='relu')(conv3_in)
+        # Ramo 2
+        conv3_in = Conv2D(f2_in, (1,1), padding='same', use_bias=False)(x)
+        conv3_in = BatchNormalization()(conv3_in)
+        conv3_in = Activation('relu')(conv3_in)
+        conv3 = Conv2D(f2_out, (3,3), padding='same', use_bias=False)(conv3_in)
+        conv3 = BatchNormalization()(conv3)
+        conv3 = Activation('relu')(conv3)
 
-        conv5_in = Conv2D(f3_in, (1,1), padding='same', activation='relu')(x)
-        conv5 = Conv2D(f3_out, (5,5), padding='same', activation='relu')(conv5_in)
+        # Ramo 3
+        conv5_in = Conv2D(f3_in, (1,1), padding='same', use_bias=False)(x)
+        conv5_in = BatchNormalization()(conv5_in)
+        conv5_in = Activation('relu')(conv5_in)
+        conv5 = Conv2D(f3_out, (5,5), padding='same', use_bias=False)(conv5_in)
+        conv5 = BatchNormalization()(conv5)
+        conv5 = Activation('relu')(conv5)
 
+        # Ramo 4
         pool = MaxPooling2D((3,3), strides=(1,1), padding='same')(x)
-        pool_proj = Conv2D(f4_out, (1,1), padding='same', activation='relu')(pool)
+        pool_proj = Conv2D(f4_out, (1,1), padding='same', use_bias=False)(pool)
+        pool_proj = BatchNormalization()(pool_proj)
+        pool_proj = Activation('relu')(pool_proj)
 
+        # A BN final após a concatenação é agora redundante e pode ser removida.
         out = Concatenate(axis=-1)([conv1, conv3, conv5, pool_proj])
-        out = BatchNormalization()(out)
         return out
 
+
+    #  ----- DEFINIÇÃO DA ARQUITETURA GERAL ----- 
+ 
     input_layer = Input(shape=(128, 128, 3))
 
-    # Dropout leve para desligar 20% dos neurônios da camada inicial.
-    x = Dropout(0.2)(input_layer)
-
-    x = Conv2D(32, (3,3), activation='relu', strides=(2,2), padding='same')(x)
+    # Convolução que produz um tensor (128,128,32)
+    # padding serve para 
+    x = Conv2D(32, (3,3), strides=(1,1), padding='same', use_bias=False)(input_layer) # use_bias=False (ver ponto 3)
+    
+    # BatchNormalization para 
     x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+
+    # Reduz a imagem de 128x128 para 64x64
     x = MaxPooling2D((2,2), strides=(2,2), padding='same')(x)
 
     x = inception_module(x, 64, 96, 128, 16, 32, 32)
-    # Dropout para desligar 30% dos neurônios da camada inception.
-    # Isso ajuda a evitar dependência de certos neur
-    x = Dropout(0.3)(x) 
 
     x = inception_module(x, 128, 128, 192, 32, 96, 64)
-    x = Dropout(0.3)(x)
 
     x = MaxPooling2D((2,2), strides=(2,2), padding='same')(x)
 
